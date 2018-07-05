3dc13bc2c62f1c933c47f3a141a9c65e188659dd
gamranov <gamraniyoussef@gmail.com>
2018-07-05 20:26:26 +0100

    initial commit

---
+++ Server/src/main/java/com/gam/project/GamProjectApplication.java
@@ -0,0 +1,12 @@
+package com.gam.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamProjectApplication.class, args);
+	}
+}
