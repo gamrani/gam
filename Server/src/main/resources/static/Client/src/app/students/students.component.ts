9c3c17427598f3e537a7d6065e500b338f12b1e2
gamranov <gamraniyoussef@gmail.com>
2018-07-07 22:51:43 +0100

    customize student component

--- Server/src/main/resources/static/Client/src/app/students/students.component.ts
+++ Server/src/main/resources/static/Client/src/app/students/students.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Student} from './Student';
 
 @Component({
   selector: 'app-students',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
-
+  persons: Student[] = [];
+  showStudent:boolean;
   constructor() { }
-
   ngOnInit() {
+    this.showStudent=true;
+    this.persons= [new Student(1,"a","b",2,"dd",3,4),new Student(2,"c","d",3,"dcd",4,4)];
   }
 
+  addStudent(){this.showStudent=false;}
+  deleteStudent(id){console.log("Supprimer l'utilisateur"+id)}
+  editStudent(id){console.log("modifier l'utilsiateur"+id)}
+  studentInformations(id){console.log("info de student"+id)}
 }
