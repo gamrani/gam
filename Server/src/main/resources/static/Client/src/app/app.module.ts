42751f5296047ba24107b2f259e818a48d157161
gamrani <gamraniyoussef@gmail.com>
2018-07-09 12:23:43 +0100

    Update design

--- Server/src/main/resources/static/Client/src/app/app.module.ts
+++ Server/src/main/resources/static/Client/src/app/app.module.ts
@@ -4,15 +4,25 @@ import {StudentService} from './services/student.service';
 import { AppComponent } from './app.component';
 import { StudentsComponent } from './students/students.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentsComponent
+    StudentsComponent,
+    MyNavComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [StudentService],
   bootstrap: [AppComponent]
